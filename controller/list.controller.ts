6a00b5816dd15f3e47ab7c247fab850aec27514c
kanha dewangan <kanhadewangan681@gmail.com>
2026-04-10 15:34:05 +0530

    add list

--- controller/list.controller.ts
+++ controller/list.controller.ts
@@ -61,3 +61,64 @@ export const updatePosition = async (req : Request, res : Response) => {
     }
 }
 
+export const deleteList = async (req : Request, res : Response) => {
+    const {listId} = req.params;
+    const list = new lists("", 0);
+    try {
+        const deletedList = await list.deleteList(listId as string);
+        if(deletedList === "No List Found") {
+            res.status(404).json({message: "No List Found"});
+        }
+        else {
+            res.status(200).json({message: "List Deleted Successfully"});
+        }
+    } catch (error: any) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const getListsByBoardId = async (req : Request, res : Response) => {
+    const {boardId} = req.params;
+    const list = new lists("", 0);
+    try {
+        const foundLists = await list.getListsByBoardId(boardId as string);
+        if(!foundLists) {
+            res.status(404).json({message: "No Lists Found"});
+        } else {
+            res.status(200).json(foundLists);
+        }
+    } catch (error: any) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const getListById = async (req : Request, res : Response) => {
+    const {listId} = req.params;
+    const list = new lists("", 0);
+    try {
+        const foundList = await list.getListById(listId as string);
+        if(!foundList) {
+            res.status(404).json({message: "No List Found"});
+        } else {
+            res.status(200).json(foundList);
+        }
+    } catch (error: any) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+
+export const getAllList = async (req : Request, res : Response) => {
+    const list = new lists("", 0);
+    try {
+        const userId = (req as any).user.id;
+        const foundLists = await list.getAllList(userId);
+        if(!foundLists) {
+            res.status(404).json({message: "No Lists Found"});
+        } else {
+            res.status(200).json(foundLists);
+        }
+    } catch (error: any) {
+        res.status(500).json({message: error.message});
+    }
+}
